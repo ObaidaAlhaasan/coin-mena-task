bebe44297d61a2caae5348327f3c5b28419f8764
Obaida616 <obaidaalhassan616@gmail.com>
2022-01-18 00:34:34 +0200

    basic fetching assets from API

--- src/pages/home/components/crypto-assets-table/crypto-assets-table.tsx
+++ src/pages/home/components/crypto-assets-table/crypto-assets-table.tsx
@@ -19,9 +19,27 @@ function LoadingError(props: { title: string }) {
   return <h3>Ops, something went wrong, failed to load {props.title} please try again later</h3>;
 }
 
+interface ICryptoAsset {
+  id: string;
+  slug: string;
+  symbol: string;
+  metrics: {
+    market_data: {
+      price_usd: number;
+    };
+  };
+}
+
+export interface ICryptoAssetResponse {
+  data: Array<ICryptoAsset>;
+  status: {
+    elapsed: number;
+    timestamp: Date | string;
+  };
+}
+
 const CryptoAssetsTable: FC<ICryptoAssetsTable> = () => {
-  const {data, status} = useQuery("crypto-assets", fetchCryptos);
-  console.log(data);
+  const {data: response, status} = useQuery<ICryptoAssetResponse>("crypto-assets", fetchCryptos);
 
   if (status === "loading")
     return <LoadingSpinner/>
@@ -31,7 +49,11 @@ const CryptoAssetsTable: FC<ICryptoAssetsTable> = () => {
 
   return (
     <div>
-
+      {response?.data?.map(c => <li>
+        <span>name : {c.slug}</span>
+        <span>price : {c.metrics.market_data.price_usd}</span>
+        <span>symbol : {c.symbol}</span>
+      </li>)}
     </div>
   );
 };
--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,3 +1,3 @@
 export const ExternalUrlsConstants = {
-  CryptoAssets: ""
+  CryptoAssets: "https://data.messari.io/api/v1/assets?fields=id,slug,symbol,metrics/market_data/price_usd"
 } as const;
