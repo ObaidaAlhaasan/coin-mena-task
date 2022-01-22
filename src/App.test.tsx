66ebe7adfd7bd9ed75881f1eb893221241c39cf7
Obaida616 <obaidaalhassan616@gmail.com>
2022-01-22 16:58:02 +0200

    add test for Trade

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Home/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Home/i)).toBeInTheDocument();
+  expect(screen.getByText(/Trade/i)).toBeInTheDocument();
 });
